0ac330e538bb157e53e02e3d46ac07a1496aba61
Brian Millar <amb3r@protonmail.ch>
2016-07-13 14:59:26 +0100

    Updated Sync

--- sync.sh
+++ sync.sh
@@ -2,18 +2,24 @@
 
 rm -rf ./sfdassm
 rm -rf ./sfdcc
+rm -rf ./sfddrivers
 rm -rf ./sfdgames
 rm -rf ./sfdlibc
+rm -rf ./sfdshell
 rm -rf ./sfdutils
 
 cp -r ../sfdassm  ./
 cp -r ../sfdcc    ./
+cp -r ../sfddrivers ./
 cp -r ../sfdgames ./
 cp -r ../sfdlibc  ./
+cp -r ../sfdshell ./
 cp -r ../sfdutils ./
 
 rm -rf ./sfdassm/.git
 rm -rf ./sfdcc/.git
+rm -rf ./sfddrivers/.git
 rm -rf ./sfdgames/.git
 rm -rf ./sfdlibc/.git
+rm -rf ./sfdshell/.git
 rm -rf ./sfdutils/.git
