166be6cbc02494f4d4447305fc0e3fd3a9f697f1
Brian Millar <amb3r@protonmail.ch>
2017-05-31 19:23:02 +0100

    Added Files

---
+++ src/progs/dev/cc/modules/preproc.c
@@ -0,0 +1,27 @@
+/* preproc.c - This file is part of sfdcc, the C Compiler of the Sandford
+ * Operating System.
+ * Copyright 2016, 2017 Brian Millar <brianmillar@protonmail.ch>
+ *
+ * sfdcc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sfdcc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sfdcc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+FILE* sfd_preproc(FILE*, char*);
+
+FILE* sfd_preproc(FILE *in, char *name) {
+    FILE *out;
+    remove(name);
+    out = fopen(name, "w");
+    fclose(in);
+    return out;
+}
---
+++ src/progs/sec/av/sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ../sfdlibs/sfdlib_file.c ./sfdlibs/sfdlib_file.c
