54b04499dfda71a640a3635345e58a7dc5a28ed4
Brian Millar <amb3r@protonmail.ch>
2017-07-29 15:53:02 +0100

    Small Improvements

--- build.sh
+++ build.sh
@@ -15,7 +15,10 @@ then
     mkdir "./documentation"
 fi
 
+echo "Test Building Libraries..."
 gcc buildtest.c -o buildtest
+
+echo "Building Utilities..."
 gcc ./source/programs/utilities/sfd_cat.c -o ./programs/utilities/sfd_cat
 gcc ./source/programs/utilities/sfd_echo.c -o ./programs/utilities/sfd_echo
 gcc ./source/programs/utilities/sfd_false.c -o ./programs/utilities/sfd_false
@@ -23,7 +26,8 @@ gcc ./source/programs/utilities/sfd_id3view.c -o ./programs/utilities/sfd_id3vie
 gcc ./source/programs/utilities/sfd_true.c -o ./programs/utilities/sfd_true
 gcc ./source/programs/utilities/sfd_yes.c -o ./programs/utilities/sfd_yes
 
-pdflatex -output-directory="./documentation" ./source/documentation/sfd_manual.tex
+echo "Building Documentation..."
+pdflatex -output-directory="./documentation" ./source/documentation/sfd_manual.tex >> /dev/null
 rm ./documentation/sfd_manual.aux
 rm ./documentation/sfd_manual.log
 rm ./documentation/sfd_manual.toc
