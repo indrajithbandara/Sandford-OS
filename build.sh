84a8c1bf064761ec4c5e04d6d688e509a4f13736
Brian Millar <amb3r@protonmail.ch>
2017-07-29 15:14:30 +0100

    Small Improvements

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+if [ ! -e './programs' ]
+then
+    mkdir "programs"
+fi
+
+if [ ! -e './programs/utilities' ]
+then
+    mkdir "./programs/utilities"
+fi
+
+gcc buildtest.c -o buildtest
+gcc ./source/programs/utilities/sfd_cat.c -o ./programs/utilities/sfd_cat
+gcc ./source/programs/utilities/sfd_echo.c -o ./programs/utilities/sfd_echo
+gcc ./source/programs/utilities/sfd_false.c -o ./programs/utilities/sfd_false
+gcc ./source/programs/utilities/sfd_id3view.c -o ./programs/utilities/sfd_id3view
+gcc ./source/programs/utilities/sfd_true.c -o ./programs/utilities/sfd_true
+gcc ./source/programs/utilities/sfd_yes.c -o ./programs/utilities/sfd_yes
--- source/programs/utilities/sfd_id3view.c
+++ source/programs/utilities/sfd_id3view.c
@@ -16,6 +16,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../../libraries/files/filetypes/sfd_libid3.c"
 
 int main(int argc, char *argv[]) {
